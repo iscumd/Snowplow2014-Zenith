c92c5b7658e83d385658aa053dfdc6888265adb1
kyesh <kenyesh@gmail.com>
2013-12-26 16:08:15 -0500

    Cleaned Up Code

--- Edge_Functions.hpp
+++ Edge_Functions.hpp
@@ -7,14 +7,16 @@
 
 void GetHorizontalEdges(cv::Mat &in, cv::Mat &out){
 	
-	int ddepth = CV_16S;
+	int ddepth = CV_16S;//Covert to Signed so we have negative and positive values
 	cv::Point anchor = cv::Point( -1, -1 );
  	double delta = 0;
 
+	//Set Data for Matrix
 	float a[9]={    -1.0,	0,	1.0,	
 	                -2.0,	0,	2.0,	
 	                -1.0,	0,	1.0};
-	                
+	
+	//Creat matrix to use as Kernel 
 	cv::Mat horizontal_kernel( 3, 3, CV_32F, a);
 	
 	filter2D(in, out, ddepth , horizontal_kernel, anchor, delta, cv::BORDER_DEFAULT );
@@ -22,14 +24,16 @@ void GetHorizontalEdges(cv::Mat &in, cv::Mat &out){
 
 void GetVerticalEdges(cv::Mat &in, cv::Mat &out){
 	
-	int ddepth = CV_16S;
+	int ddepth = CV_16S;//Convert to signed so we have negative and positive values
 	cv::Point anchor = cv::Point( -1, -1 );
  	double delta = 0;
  	
- 	float b[9]={ 	1.0,	2,	1.0,	
+ 	//Set Data for Matrix
+	float b[9]={ 	1.0,	2,	1.0,	
 	                0,	0,	0,	
 	               -1.0,	-2,	-1.0};
 	            
+	//Creat matrix to use as Kernel 
 	cv::Mat vertical_kernel( 3, 3, CV_32F, b);
 	
 	filter2D(in, out, ddepth , vertical_kernel , anchor, delta, cv::BORDER_DEFAULT );   
