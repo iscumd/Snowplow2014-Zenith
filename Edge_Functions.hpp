084822ec524e1a641d5bd7d1dd9dea3945a4cee6
kyesh <kenyesh@gmail.com>
2013-12-26 15:13:34 -0500

    Fixed Comile Bugs
    Forgot to use CV name space

--- Edge_Functions.hpp
+++ Edge_Functions.hpp
@@ -4,33 +4,34 @@
 *Purpouse: Functions used in determining edges and their location 
 *********************************************/
 
-Void GetHorizontalEdges(Mat in, Mat out){
+
+void GetHorizontalEdges(cv::Mat in, cv::Mat out){
 	
-	ddepth = CV_16S;
-	anchor = Point( -1, -1 );
- 	delta = 0;
+	int ddepth = CV_16S;
+	cv::Point anchor = cv::Point( -1, -1 );
+ 	double delta = 0;
 
 	float a[9]={    -1.0,	0,	1.0,	
 	                -2.0,	0,	2.0,	
 	                -1.0,	0,	1.0};
 	                
-	Mat horizontal_kernel( 3, 3, CV_32F, a);
+	cv::Mat horizontal_kernel( 3, 3, CV_32F, a);
 	
-	filter2D(in, out, ddepth , horizontal_kernel, anchor, delta, BORDER_DEFAULT );
+	filter2D(in, out, ddepth , horizontal_kernel, anchor, delta, cv::BORDER_DEFAULT );
 }
 
-Void GetVerticalEdges(Mat in, Mat out){
+void GetVerticalEdges(cv::Mat in, cv::Mat out){
 	
-	ddepth = CV_16S;
-	anchor = Point( -1, -1 );
- 	delta = 0;
+	int ddepth = CV_16S;
+	cv::Point anchor = cv::Point( -1, -1 );
+ 	double delta = 0;
  	
  	float b[9]={ 	1.0,	2,	1.0,	
 	                0,	0,	0,	
 	               -1.0,	-2,	-1.0};
 	            
-	Mat vertical_kernel( 3, 3, CV_32F, b);
+	cv::Mat vertical_kernel( 3, 3, CV_32F, b);
 	
-	filter2D(in, out, ddepth , vertical_kernel , anchor, delta, BORDER_DEFAULT );   
+	filter2D(in, out, ddepth , vertical_kernel , anchor, delta, cv::BORDER_DEFAULT );   
 	               
 }
